bec16a8716bffc608ae78fff85dab9e9fd5b0c51
sabikunnahar16 <sabikunnahar789789@gmail.com>
2025-01-11 18:33:16 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://dev.ontrak.onsite.com.au/dashboard");
+Thread.Sleep(10000);
+
+driver.Manage().Window.Maximize();
+driver.FindElement(By.Id("Username")).SendKeys("ju");
+driver.FindElement(By.Id("btn-login")).Click();
+driver.FindElement(By.Id("Password")).SendKeys("ju");
+driver.FindElement(By.Id("btn-login")).Click();
+
+Thread.Sleep(10000);
+
+//driver.FindElement(By.Id("connect-dashboard-v2")).Click();
+driver.FindElement(By.CssSelector("#toolbar-outer-extended > div > div.dx-toolbar-after > div:nth-child(1)")).Click();
+
