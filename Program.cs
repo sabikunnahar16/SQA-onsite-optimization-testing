12628b33ed506642683b38126525606e1e5f2586
sabikunnahar16 <sabikunnahar789789@gmail.com>
2025-01-12 16:33:55 +0600

    new commit

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ Thread.Sleep(10000);
 driver.Manage().Window.Maximize();
 driver.FindElement(By.Id("Username")).SendKeys("ju");
 driver.FindElement(By.Id("btn-login")).Click();
+Thread.Sleep(10000);
 driver.FindElement(By.Id("Password")).SendKeys("ju");
 driver.FindElement(By.Id("btn-login")).Click();
 
 Thread.Sleep(10000);
 
-//driver.FindElement(By.Id("connect-dashboard-v2")).Click();
-driver.FindElement(By.CssSelector("#toolbar-outer-extended > div > div.dx-toolbar-after > div:nth-child(1)")).Click();
+driver.FindElement(By.Id("connect-dashboard-v2")).Click();
+//driver.FindElement(By.CssSelector("#toolbar-outer-extended > div > div.dx-toolbar-after > div:nth-child(1)")).Click();
 
